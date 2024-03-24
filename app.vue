b82784caf992d296db8764ed3bea2e863e8d584e
zikumanage <suzu.kyo@zikumanage.co.jp>
2024-03-24 22:21:27 +0900

    1

--- app.vue
+++ app.vue
@@ -1,8 +1,103 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is update
-    </AppAlert>
-  </div>
+  <div class="scroll_container">
+      <div class="sticky_wrap">
+        <div class="horizontal_scroll">
+          <div class="scroll_contents red">
+            <h2 class="left">Hello</h2>
+          </div>
+          <div class="scroll_contents yellow"></div>
+          <div class="scroll_contents green"></div>
+          <div class="scroll_contents blue">
+            <h2 class="right">Goodbye</h2>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+
+const transform = (section) => {
+  const offsetTop = section.parentElement.offsetTop;
+
+  const scrollSection = section.querySelector('.horizontal_scroll')
+
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+
+  percentage = percentage < 0 ? 0 : percentage > 300 ? 300 : percentage;
+
+  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
+};
+
+onMounted(() => {
+  const stickySections = [...document.querySelectorAll('.sticky_wrap')]
+  
+  window.addEventListener('scroll', (e) => {
+    for(let i = 0; i < stickySections.length; i++){
+      transform(stickySections[i])
+    }
+  })
+})
+
+</script>
+
+<style lang="scss">
+
+.scroll_container {
+  height: 400vh;
+}
+
+.horizontal_scroll {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 400vw;
+  will-change: transform;
+  display: flex;
+  justify-content: space-between;
+}
+
+.scroll_contents {
+  height: 100%;
+  width: 100vw;
+}
+
+.sticky_wrap {
+  overflow: hidden;
+  position: sticky;
+  top: 0;
+  height: 100vh;
+}
+
+.red {
+  background-color: #ff3d00;
+}
+
+.yellow {
+  background-color: #ffff00;
+}
+
+.green {
+  background-color: #05ff00;
+}
+
+.blue {
+  background-color: #2835f8;
+}
+
+h2 {
+  font-size: 2rem;
+  color: #f7f7f7;
+}
+
+.left {
+  margin-top: 5vh;
+  margin-left: 5vw;
+}
+
+.right {
+  text-align: right;
+  margin-top: 85vh;
+  margin-right: 5vw;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/css/_variables.scss";',
+        },
+      },
+    },
+  },
+  css: ["@/assets/css/main.scss"],
+  app:{
+    head: {
+    title: 'JAICO',
+    htmlAttrs: {
+      lang : 'ja'
+    },
+    script: [
+      { src: 'https://platform.twitter.com/widgets.js' }
+    ],
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'JAICO' },
+      { hid: 'og:image', property: 'og:image', content: 'https://' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=M+PLUS+Rounded+1c:wght@500;800&family=Noto+Sans+JP:wght@400;700&display=swap' }
+    ]
+  },
+  }
 })
